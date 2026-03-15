e5de4891c372f8e5e049ff2a315ec82e4cbf198a
Ubuntu <ubuntu@ip-172-31-19-134.ec2.internal>
2026-03-15 11:17:05 +0000

    webhook test

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/api')
-def api():
-    return {"message":"Hello from Flask backend"}
+@app.route("/")
+def home():
+    return "Flask Backend Running successfully!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
