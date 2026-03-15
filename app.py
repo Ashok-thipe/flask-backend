50114c2f3f21bd94ee478a39618c3646a269cfa2
Ubuntu <ubuntu@ip-172-31-19-134.ec2.internal>
2026-03-15 12:13:14 +0000

    made changes in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Backend Running (successfully)!"
+    return "Flask Backend Running successfully!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
